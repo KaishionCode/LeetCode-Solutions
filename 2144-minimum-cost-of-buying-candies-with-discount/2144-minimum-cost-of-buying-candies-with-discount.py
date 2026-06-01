19851500e505e26652639781d69777e48ae3c627
KaishionCode <kaishion.work@gmail.com>
2026-06-01 07:16:19 +0700

    Time: 0 ms (100%), Space: 19.2 MB (48.06%) - LeetHub - Time: O(?), Space: O(?)

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
@@ -1,12 +1,7 @@
 class Solution:
     def minimumCost(self, cost: List[int]) -> int:
-        luutru = (sorted(cost)[::-1])
-        tong = 0
-        for i in range (len(luutru)):
-            if (i + 1) % 3 == 0:
-                continue
-            else: tong += luutru[i]
-        return tong
+        cost.sort(reverse = True)
+        return sum(cost) - sum(cost[2::3])
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
