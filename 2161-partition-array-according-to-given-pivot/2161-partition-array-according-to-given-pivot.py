f7fd2ad300bbff397ac4aee7b2fb299edcde9841
KaishionCode <kaishion.work@gmail.com>
2026-06-08 07:39:27 +0700

    Time: 31 ms (58.89%), Space: 33.8 MB (55.83%) - LeetHub - Time: O(?), Space: O(?)

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -1,14 +1,8 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        vetrai = []
-        vegiua = []
-        vephai = []
-        for i in nums:
-            if i < pivot: vetrai.append(i)
-            if i == pivot: vegiua.append(i)
-            if i > pivot: vephai.append(i)
-        ketqua = vetrai+vegiua+vephai
-        return ketqua
+        return [x for x in nums if x < pivot] + \
+               [x for x in nums if x == pivot] + \
+               [x for x in nums if x > pivot]
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
