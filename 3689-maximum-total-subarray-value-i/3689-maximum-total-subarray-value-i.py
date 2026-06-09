76e2a48c82dca4c999f5a411b7650c09227104a7
KaishionCode <kaishion.work@gmail.com>
2026-06-09 09:12:44 +0700

    Time: 20 ms (28.8%), Space: 26.4 MB (60%) - LeetHub - Time: O(?), Space: O(?)

--- 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.py
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.py
@@ -1,6 +1,11 @@
 class Solution:
     def maxTotalValue(self, nums: List[int], k: int) -> int:
-        return (max(nums) - min(nums))*k
+        min_len = float ('inf')
+        max_len = float ('-inf')
+        for i in nums:
+            if i < min_len: min_len = i
+            if i > max_len: max_len = i
+        return (max_len - min_len) * k
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
